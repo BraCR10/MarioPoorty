e9c19cc5a9abd2bab878231cf7e82c9485499343
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-10-24 15:55:57 -0600

    First Commit

---
+++ MarioPoorty/src/main/java/poo/mariopoorty/MarioPoorty.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.mariopoorty;
+
+/**
+ *
+ * @author Brian
+ */
+public class MarioPoorty {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
